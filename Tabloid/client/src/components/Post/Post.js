39bb898e2f6bd00dbfe7686f099af3be4464f823
Meg McGregor <megmcgregor1@gmail.com>
2021-07-15 11:46:12 -0700

    only approved posts displaying, title author and category, sort by date

--- Tabloid/client/src/components/Post/Post.js
+++ Tabloid/client/src/components/Post/Post.js
@@ -2,14 +2,16 @@ import React from "react";
 import { Card, CardBody } from "reactstrap";
 
 const Post = ({ post }) => {
+    console.log(post.userProfile.displayName)
     return (
         <Card >
             < CardBody >
                 <p>
                     <strong>{post.title}</strong>
                 </p>
-                <p className="text-left px-2">Posted by: {post.userProfile}</p>
+                <p> Posted by:{post.userProfile.displayName}</p>
                 <p>{post.content}</p>
+                <p>{post.category.name}</p>
             </CardBody >
         </Card >
     );
