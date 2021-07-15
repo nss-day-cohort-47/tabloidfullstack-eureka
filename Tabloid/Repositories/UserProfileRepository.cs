7f8202833e01f0248f64e1c32fb292b9a633576e
prestonshotts <shotts.preston@gmail.com>
2021-07-15 09:52:34 -0700

    second commit for backend

--- Tabloid/Repositories/UserProfileRepository.cs
+++ Tabloid/Repositories/UserProfileRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
 using Tabloid.Models;
 using Tabloid.Utils;
 
@@ -7,6 +8,50 @@ namespace Tabloid.Repositories
     public class UserProfileRepository : BaseRepository, IUserProfileRepository
     {
         public UserProfileRepository(IConfiguration configuration) : base(configuration) { }
+
+
+        public List<UserProfile> GetAllUserProfiles()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+               SELECT up.Id, up.FirebaseUserId, up.DisplayName, up.FirstName, up.LastName, 
+                      up.Email, up.CreateDateTime,
+                      up.ImageLocation, up.UserTypeId, up.IsDeleted
+                      
+                 FROM UserProfile up 
+                 WHERE IsDeleted=0
+                ";
+
+                    var reader = cmd.ExecuteReader();
+
+                    var UserProfiles = new List<UserProfile>();
+                    while (reader.Read())
+                    {
+                        UserProfiles.Add(new UserProfile()
+                        {
+                            Id = DbUtils.GetInt(reader, "Id"),
+                            FirebaseUserId = DbUtils.GetString(reader, "FirebaseUserId"),
+                            DisplayName = DbUtils.GetString(reader, "DisplayName"),
+                            FirstName = DbUtils.GetString(reader, "FirstName"),
+                            LastName = DbUtils.GetString(reader, "LastName"),
+                            Email = DbUtils.GetString(reader, "Email"),
+                            CreateDateTime = DbUtils.GetDateTime(reader, "CreateDateTime"),
+                            ImageLocation = DbUtils.GetString(reader, "ImageLocation"),
+                            UserTypeId = DbUtils.GetInt(reader, "UserTypeId"),
+                            IsDeleted = reader.GetBoolean(reader.GetOrdinal("IsDeleted"))
+                        });
+                    }
+                    
+                    reader.Close();
+
+                    return UserProfiles;
+                }
+            }
+        }
 
         public UserProfile GetByFirebaseUserId(string firebaseUserId)
         {
